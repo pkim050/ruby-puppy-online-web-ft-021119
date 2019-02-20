c744308178a13827c1272ae5fc36f4387e37d8c3
Patrick Kim <pkim050@gmail.com>
2019-02-20 01:51:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,12 @@ require 'pry'
 class Dog
   attr_accessor :name
   @@all = []
+  @@all2 = []
   
   def initialize(name)
     @@all << self
     @name = name
+    @@all2 << name
   end
   
   def self.all
